599010d32fee6bda8906bdda9360a57c1fbe1838
t-k- <clock126@126.com>
2020-12-03 21:49:07 +0800

    avoid installing certs everytime the service is up.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,22 +14,28 @@ set +x
 # Example: DOMAIN=approach0.xyz
 if [ -n "$DOMAIN" ]; then
 	RELOAD_CMD='nginx -p /root/ -s reload'
-
-	# Enable TLS in nginx.conf and reload httpd
+	# Enable TLS in Nginx using boostrap key/cert.pem
 	sed -i 's/# UNCOMMENT_THIS//g' ./conf/nginx.conf
 	sed -i '/DELETE_THIS/d' ./conf/nginx.conf
 	$RELOAD_CMD
-	cat ./conf/nginx.conf
 
-	pushd ./acme.sh
-	# Verify and issue certificate
-	./acme.sh --issue -d $DOMAIN -d www.$DOMAIN -w /root
-	# Generate certificate pem files
-	./acme.sh --install-cert -d $DOMAIN -d www.$DOMAIN \
-		--key-file /root/key.pem \
-		--fullchain-file /root/cert.pem \
-		--reloadcmd "$RELOAD_CMD"
-	popd
+	# try to reload a real key/cert.pem, install it if it's missing.
+	CERT_DIR='/root/keys'
+	UPDATE_CERT="cp $CERT_DIR/key.pem /root; cp $CERT_DIR/cert.pem /root; $RELOAD_CMD"
+	if [ -e $CERT_DIR/key.pem ]; then
+		$UPDATE_CERT
+	else
+		pushd ./acme.sh
+		# Verify and issue certificate
+		./acme.sh --issue -d $DOMAIN -d www.$DOMAIN -w /root
+		# Generate certificate pem files
+		mkdir -p $CERT_DIR
+		./acme.sh --install-cert -d $DOMAIN -d www.$DOMAIN \
+			--key-file $CERT_DIR/key.pem \
+			--fullchain-file $CERT_DIR/cert.pem \
+			--reloadcmd "$UPDATE_CERT"
+		popd
+	fi
 
 	# We should have timer job to renew certification now
 	crontab -l
